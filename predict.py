4e4eff13f06e949331aa092946e3f7a54c2c4947
Eva de Schipper <eva.deschipper@cito.nl>
2020-03-14 23:06:31 +0100

    WIP predict ideas for tomorrow

---
+++ predict.py
@@ -0,0 +1,5 @@
+# Training can be done with all of the data because prediction will basically be done based on our input?
+# So define train function
+# Make predict function based on probabilities derived from training the data
+# sklearn or nlptk for training ngrams?
+
