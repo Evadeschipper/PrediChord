036b40cf41e92594e809b1f6f4f0d962039cdf6d
Eva de Schipper <eva.deschipper@cito.nl>
2020-03-21 17:15:30 +0100

    WIP chord_to_notes

---
+++ sound.py
@@ -0,0 +1,14 @@
+
+def chord_to_notes(chord):
+
+    """
+    Finds the notes belonging to the chord to write to midi file. 
+
+    Args:
+        chord: str
+  
+    Returns:
+        notes: tuple of strings. 
+    """
+
+    return(notes)
