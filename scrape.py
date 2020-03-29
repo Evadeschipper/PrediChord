77ea963f21e0fafbf5b95ab94fc1f06933277d5f
Eva de Schipper <eva.deschipper@cito.nl>
2020-03-29 16:44:58 +0200

    skip repeated artist names and official versions

--- scrape.py
+++ scrape.py
@@ -11,6 +11,54 @@ from selenium.webdriver.chrome.options import Options
 from selenium import webdriver
 from transpose import equiChord
 
+def scrape_csv(fp):
+    df = pd.read_csv(fp, sep=";")
+    for i, row in df.iterrows():
+        song_name, artist = row[["song-name", "artist"]]
+
+def scrape_streaming_data(datapath):
+    """
+    Scrape streaming data from a spotify datadump. 
+
+    Args:
+        datapath: string - path of folder with spotify datadump.
+    
+    Returns:
+        df: pandas df with the streaming data. 
+    """
+
+    r = re.compile("StreamingHistory")
+    jsonfiles = list(filter(r.match, listdir(datapath)))
+
+    jsonlist = []
+
+    for jsonfile in jsonfiles:
+        with open(datapath + "/" + jsonfile, encoding="utf8") as f:
+            jsonlist += json.load(f)
+
+    endTime = []
+    artistName = []
+    trackName = []
+    msPlayed = []
+
+    for play in jsonlist:
+        endTime.append(play.get('endTime'))
+        artistName.append(play.get('artistName'))
+        trackName.append(play.get('trackName'))
+        msPlayed.append(play.get('msPlayed'))
+
+    data = {'endTime':  endTime,
+            'artistName': artistName,
+            'trackName': trackName,
+            'msPlayed': msPlayed}
+
+    df = pd.DataFrame(data, columns = ['endTime','artistName', 'trackName', 'msPlayed'])
+
+    # Throw out plays that lasted less than a minute.
+    df = df[df['msPlayed'] > 60000]
+
+    return df
+
 def __scrape_chords(html):
     """
     Extract the chords from an ultimate-guitar html template.
@@ -51,15 +99,24 @@ def __scrape_matches(html):
     # Code below relies on that consistency
 
     soup = BeautifulSoup(html, features="html.parser")
+    cur_artist = None
     # Skip the header row of the results
     for result_line in soup.find_all(attrs={"class": "pZcWD"})[1:]:
         artist, song, rating, result_type = result_line.contents
-
-        artist = artist.find("a").text
+        
+        if artist.find("a") is not None:
+            artist = artist.find("a").text
+            cur_artist = artist
+        else:
+            artist = cur_artist
         
         song_a_tag = song.find("a")
         song = song_a_tag.text
         chord_url = song_a_tag["href"]
+
+        # Skip official version of song on UG.
+        if rating.find(attrs={"class": "dEQ1I"}) is None:
+            continue
 
         stars, n_raters = rating.find(attrs={"class": "dEQ1I"}), rating.find(attrs={"class": "_31dWM"}).text
 
@@ -208,82 +265,13 @@ def scrape_song(song_name, artist, force_rescrape=False):
     
     return chords
 
-
-def scrape_csv(fp):
-    df = pd.read_csv(fp, sep=";")
-    for i, row in df.iterrows():
-        song_name, artist = row[["song-name", "artist"]]
-
-def scrape_streaming_data(datapath):
-    """
-    Scrape streaming data from a spotify datadump. 
-
-    Args:
-        datapath: string - path of folder with spotify datadump.
-    
-    Returns:
-        df: pandas df with the streaming data. 
-    """
-
-    r = re.compile("StreamingHistory")
-    jsonfiles = list(filter(r.match, listdir(datapath)))
-
-    jsonlist = []
-
-    for jsonfile in jsonfiles:
-        with open(datapath + "/" + jsonfile, encoding="utf8") as f:
-            jsonlist += json.load(f)
-
-    endTime = []
-    artistName = []
-    trackName = []
-    msPlayed = []
-
-    for play in jsonlist:
-        endTime.append(play.get('endTime'))
-        artistName.append(play.get('artistName'))
-        trackName.append(play.get('trackName'))
-        msPlayed.append(play.get('msPlayed'))
-
-    data = {'endTime':  endTime,
-            'artistName': artistName,
-            'trackName': trackName,
-            'msPlayed': msPlayed}
-
-    df = pd.DataFrame(data, columns = ['endTime','artistName', 'trackName', 'msPlayed'])
-
-    # Throw out plays that lasted less than a minute.
-    df = df[df['msPlayed'] > 60000]
-
-    return df
-
 if __name__ == "__main__":
+
+    chords = scrape_song("When I was your man", "Bruno Mars", force_rescrape = True)
+    print(chords)
+
     """   df = scrape_streaming_data("./MyData")
 
     # Plays per track
     trackCounts = df.groupby('artistName')['trackName'].value_counts() """
-
-    chrome_options = Options()
-    chrome_options.add_argument("--headless")
-  
-    if sys.platform.startswith("linux"):
-        if op.exists(op.join(op.dirname(__file__), "chromedrivers")):
-            driver = webdriver.Chrome(chrome_options=chrome_options, executable_path=op.join(op.dirname(__file__), "chromedrivers", "chromedriver"))
-        else:
-            driver = webdriver.Chrome(chrome_options=chrome_options, executable_path=op.join(op.dirname(__file__), "chromedriver"))
-    elif sys.platform.startswith("win32"):
-        if op.exists(op.join(op.dirname(__file__), "chromedrivers")):
-            driver = webdriver.Chrome(chrome_options=chrome_options, executable_path=op.join(op.dirname(__file__), "chromedrivers", "chromedriver.exe"))
-        else:
-            driver = webdriver.Chrome(chrome_options=chrome_options, executable_path=op.join(op.dirname(__file__), "chromedriver.exe"))
-    else:
-        # Try to recover with opportunistic pathing
-        driver = webdriver.Chrome(chrome_options=chrome_options)
-
-    driver.get("https://tabs.ultimate-guitar.com/tab/bruno-mars/when-i-was-your-man-chords-1198871")
-
-    chords = __scrape_chords(driver.page_source)
-
-    driver.close()
-
-    print(chords)
+    
